f9a6e4970f3885c2050e0c082701f4dffb005e5d
Windows-Explorer <danil.perfilef@yandex.ru>
2022-04-26 11:55:50 +0800

    Поправил модель

--- backend/models/artworkModel.js
+++ backend/models/artworkModel.js
@@ -4,9 +4,10 @@ import { db } from "./db"
 export const Artwork = db.define('Artwork', {
     id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
     name: { type: DataTypes.STRING, allowNull: false, },
-    author: { type: DataTypes.INTEGER, allowNull: false },
+    author: { type: DataTypes.INTEGER, allowNull: true },
     created_date: { type: DataTypes.DATEONLY, allowNull: false },
     description: { type: DataTypes.STRING, allowNull: false },
     sizeX: { type: DataTypes.DOUBLE, allowNull: false },
-    sizeY: { type: DataTypes.DOUBLE, allowNull: false }
+    sizeY: { type: DataTypes.DOUBLE, allowNull: false },
+    picture: { type: DataTypes.STRING, allowNull: false },
 }, { timestamps: false })
