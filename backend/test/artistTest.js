8d81f4b1332ca92438861b6b0774a417583bae72
Windows-Explorer <danil.perfilef@yandex.ru>
2022-04-30 09:03:48 +0800

    Дописан тест на /artist/create

--- backend/test/artistTest.js
+++ backend/test/artistTest.js
@@ -4,7 +4,9 @@ import { fastify as app } from "../main.js"
 import { artist, artwork } from "./boolshit.js"
 
 chai.use(chaiHttp)
-let artist_id
+
+let artistId
+
 describe("Artist tests", () => {
 
     it("It's should clear artist table", (done) => {
@@ -20,20 +22,23 @@ describe("Artist tests", () => {
     })
 
     it("It's should add artist to database", (done) => {
-        chai.request(app.server).post("/artist/create").send(artist).end((err, res) => {
-          assert.equal(res.status, 200)
-          assert.isArray(res.body)
-          assert.property(res.body[0], "id")
-          assert.property(res.body[0], "first_name")
-          assert.property(res.body[0], "second_name")
-          assert.property(res.body[0], "birth_date")
-          assert.property(res.body[0], "death_date")
-          assert.property(res.body[0], "image")
-          assert.property(res.body[0], "country")
-          artist_id = res.body.id
-          done()
+      chai.request(app.server).post("/artist/create").send(artist).end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.isArray(res.body)
+        assert.property(res.body[0], "id")
+        assert.property(res.body[0], "first_name")
+        assert.property(res.body[0], "second_name")
+        assert.property(res.body[0], "birth_date")
+        assert.property(res.body[0], "death_date")
+        assert.property(res.body[0], "image")
+        assert.property(res.body[0], "country")
+        artistId = res.body.id
+        chai.request(app.server).get("/artist/" + artistId).end((err, res) => {
+          assert.equal(res.body[0], artist)
         })
+        done()
       })
+    })
 
     it("It's should get all artists from database", (done) => {
       chai.request(app.server).get("/artist").end((err, res) => {
@@ -44,7 +49,7 @@ describe("Artist tests", () => {
     })
 
     it("It's should get artist by id from database", (done) => {
-        chai.request(app.server).get("/artist/" + artist_id).end((err, res) => {
+        chai.request(app.server).get("/artist/" + artistId).end((err, res) => {
           assert.equal(res.status, 200)
           assert.isArray(res.body)
           done()
@@ -52,16 +57,16 @@ describe("Artist tests", () => {
       })
 
     it("It's should update artist from database", (done) => {
-        chai.request(app.server).post("/artist/update/" + artist_id).send({death_date: "2099-12-31"}).end((err, res) => {
+        chai.request(app.server).post("/artist/update/" + artistId).send({death_date: "2099-12-31"}).end((err, res) => {
           assert.equal(res.status, 200)
           done()
         })
       })
 
     it("It's should delete artist by id from database", (done) => {
-        chai.request(app.server).get("/artist/delete/" + artist_id).end((err, res) => {
+        chai.request(app.server).get("/artist/delete/" + artistId).end((err, res) => {
           assert.equal(res.status, 200)
-          chai.request(app.server).get("/artist/" + artist_id).end((err, res) => {
+          chai.request(app.server).get("/artist/" + artistId).end((err, res) => {
             assert.equal(res.status, 200)
             assert.isArray(res.body)
             done()
