78117e093fd5485f922bd1fcba28ca51b71e267e
fordownloads <vnukov.d03@gmail.com>
2022-05-11 04:09:22 +0800

    Префикс /api

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
     apiUrl: 'http://localhost:3000',
     appName: 'GayStation',
-    authorPicUrl: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/981.jpg'
+    authorPicUrl: 'https://static.cocks.rocks/author.jpg'
 }
