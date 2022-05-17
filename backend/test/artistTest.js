dc9fe62a67dccd98753780e7fe417ea4c34c164e
fordownloads <vnukov.d03@gmail.com>
2022-05-18 00:53:20 +0800

    Merge branch 'testing'

---
+++ backend/test/artistTest.js
@@ -0,0 +1,89 @@
+import chai, { assert } from "chai"
+import chaiHttp from "chai-http"
+import { fastify as app } from "../main.js"
+import { artist, artwork } from "./models.js"
+
+chai.use(chaiHttp)
+
+let artistId
+
+describe("Artist tests", () => {
+
+    it("It's should clear artist table", (done) => {
+      chai.request(app.server).get("/artist/delete").end((err, res1) => {
+        assert.equal(res1.status, 200)
+        chai.request(app.server).get("/artist").end((err, res2) => {
+          assert.equal(res2.status, 200)
+          assert.isArray(res2.body)
+          assert.equal(res2.body.length, 0)
+          done()
+        })
+      })
+    })
+
+    it("It's should add artist to database", (done) => {
+      chai.request(app.server).post("/artist/create").send(artist).end((err, res) => {
+        assert.equal(res.status, 200)
+        artistId = res.body.id
+        chai.request(app.server).get("/artist/" + artistId).end((err, res) => {
+          assert.property(res.body[0], "id")
+          assert.property(res.body[0], "first_name")
+          assert.property(res.body[0], "second_name")
+          assert.property(res.body[0], "birth_date")
+          assert.property(res.body[0], "death_date")
+          assert.property(res.body[0], "image")
+          assert.property(res.body[0], "country")
+          done()
+        })
+      })
+    })
+
+    it("It's should get all artists from database", (done) => {
+      chai.request(app.server).get("/artist").end((err, res) => {
+        assert.equal(res.status, 200)
+        assert.isArray(res.body)
+        assert.property(res.body[0], "id")
+        assert.property(res.body[0], "first_name")
+        assert.property(res.body[0], "second_name")
+        assert.property(res.body[0], "birth_date")
+        assert.property(res.body[0], "death_date")
+        assert.property(res.body[0], "image")
+        assert.property(res.body[0], "country")
+        done()
+      })
+    })
+
+    it("It's should get artist by id from database", (done) => {
+        chai.request(app.server).get("/artist/" + artistId).end((err, res) => {
+          assert.equal(res.status, 200)
+          assert.isArray(res.body)
+          assert.property(res.body[0], "id")
+          assert.property(res.body[0], "first_name")
+          assert.property(res.body[0], "second_name")
+          assert.property(res.body[0], "birth_date")
+          assert.property(res.body[0], "death_date")
+          assert.property(res.body[0], "image")
+          assert.property(res.body[0], "country")
+          done()
+        })
+      })
+
+    it("It's should update artist from database", (done) => {
+        chai.request(app.server).post("/artist/update/" + artistId).send({ death_date: "2099-12-31" }).end((err, res) => {
+          assert.equal(res.status, 200)
+          done()
+        })
+      })
+
+    it("It's should delete artist by id from database", (done) => {
+        chai.request(app.server).get("/artist/delete/" + artistId).end((err, res) => {
+          assert.equal(res.status, 200)
+          chai.request(app.server).get("/artist/" + artistId).end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.isArray(res.body)
+            done()
+          })
+        })
+      })
+  
+  })
---
+++ backend/test/models.js
@@ -0,0 +1,19 @@
+export const artist = {
+    first_name: "Stoldman",
+    second_name: "Gref",
+    birth_date: "2090-12-21",
+    // death_date: "2000-02-01",
+    image: "jeffrey-katzenberg-has-a-howard-schultz-problem-and-heres-why-that-matters-to-you.jpg",
+    country: "otch fam im sr_ball and валуес"
+}
+
+export const artwork = {
+    name: "Castle of the mocha",
+    authorId: null,
+    created_date: "2090-01-01",
+    description: "power full rock music in picture",
+    sizeX: 100.444,
+    sizeY: 37.3423,
+    image: "somebody_once_told_me_the_world_is_gonna_roll_me.jpg",
+    location: "All stars"
+}
